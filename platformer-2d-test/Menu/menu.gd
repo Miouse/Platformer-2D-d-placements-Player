57a0eececb89f1b3a54cf950f759ba59b164dd8c
Miouse <laraigneed@gmail.com>
2025-10-19 20:25:10 +0200

    Add files via upload

---
+++ platformer-2d-test/Menu/menu.gd
@@ -0,0 +1,9 @@
+extends Control
+@onready var first_level = preload("res://Level/level.tscn")
+
+func _on_b_start_button_down() -> void:
+	get_tree().change_scene_to_packed(first_level)
+
+
+func _on_b_quit_button_down() -> void:
+	get_tree().quit()
